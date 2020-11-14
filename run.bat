b8023130454e8e8b36f50d813ab0ec6d95cad362
Ayush Mandowara <ayushxx7@gmail.com>
2020-11-15 01:48:41 +0530

    feat(run.bat): run in Admin mode by default
    using a .vbs script (created on the fly), the contents of this bat file
    will automatically execute in Admin mode (hopefully everywhere). This
    way, chocolatey and ffmpeg can be easily installed.
    Change the calling from `call` to `start` as that is required to open a
    separate command window after ffmpeg is installed.
    
    Note: Ideally, this whole process should be broken down into two steps.
    If the required modules are already installed, just run the script, else
    install the requirements first and then run. Currently, requirements are
    being installed regardless of whether they are needed or not.

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
-:: echo "Installing Chocolatey Package Manager"
-:: Powershell.exe -executionpolicy bypass -File choco_install_powershell.ps1
-:: echo "Install ffmpeg using Chocolatey. Required for merging audio and video files."
-:: call cmd /k choco install ffmpeg -y
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+echo "Installing Chocolatey Package Manager"
+Powershell.exe -executionpolicy bypass -File choco_install_powershell.ps1
+echo "Install ffmpeg using Chocolatey. Required for merging audio and video files."
+start cmd /k "choco install ffmpeg -y"
 py -m pip install -r requirements.txt
 py download_all.py
